d641a13e7ba6bbd5cf5109db221d37cc9ecf0ee1
Hubert Łakomski <hubertlakomski@gmail.com>
2019-12-19 22:34:56 +0100

    java database support

--- src/pl/Application.java
+++ src/pl/Application.java
@@ -1,48 +1,15 @@
 package pl;
 
-import pl.dao.UserDao;
-import pl.models.User;
-
-import java.util.Arrays;
-import java.util.Scanner;
+import pl.models.Menu;
 
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Użytkownicy: ");
 
-        UserDao userDao = new UserDao();
-        User[] allUsers = userDao.findAll();
+        Menu menu = new Menu();
 
-        for(User user: allUsers){
-            System.out.println(user);
-        }
-
-        String[] options = {"1. Dodanie użytkownika",
-                "2. Edycja użytkownika",
-                "3. Usunięcie użytkownika",
-                "4. Zakończenie programu"};
-
-        System.out.println("Opcje: ");
-
-        for (String option: options) {
-            System.out.println(option);
-        }
-
-        System.out.print("Wciśnij klawisz z nr opcji: ");
-
-        Scanner input = new Scanner(System.in);
-
-        while (!input.hasNextInt() || input.nextInt()>options.length || input.nextInt()<0) {
-            System.out.print("Wyborem jest liczba całkowita: ");
-            input.nextLine();
-        }
-
-        int choice = input.nextInt();
-
-        switch (choice){
-
-            case 1:
+        while(true){
+            menu.turn();
         }
 
     }
---
+++ src/pl/models/Menu.java
@@ -0,0 +1,114 @@
+package pl.models;
+
+import pl.dao.UserDao;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    public void turn(){
+        System.out.println("Użytkownicy: ");
+
+        UserDao userDao = new UserDao();
+        User[] allUsers = userDao.findAll();
+
+        for(User user: allUsers){
+            System.out.println(user);
+        }
+
+        String[] options = {"1. Dodanie użytkownika",
+                "2. Edycja użytkownika",
+                "3. Usunięcie użytkownika",
+                "4. Zakończenie programu"};
+
+        System.out.println("Opcje: ");
+
+        for (String option: options) {
+            System.out.println(option);
+        }
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Wciśnij klawisz z nr wybranej opcji: ");
+
+        int choice = input.nextInt();
+
+        if(choice==1) {
+
+            String name;
+            String email;
+            String password;
+            int user_group_id;
+
+            System.out.println("Dodawanie użytkownika");
+
+            System.out.print("1. Podaj imię: ");
+            name = input.next();
+            System.out.print("2. Podaj email: ");
+            email = input.next();
+            System.out.print("3. Podaj hasło: ");
+            password = input.next();
+            System.out.print("4. Podaj id grupy: ");
+            user_group_id = input.nextInt();
+
+            User user = new User(name, email, password, user_group_id);
+
+            userDao.create(user);
+
+            System.out.println("Użytkownik dodany..");
+        }
+
+        else if(choice==2) {
+            int userId;
+            String name;
+            String email;
+            String password;
+            int user_group_id;
+
+            System.out.println("Edycja użytkownika");
+
+            System.out.print("Podaj id użytkownika: ");
+            userId = input.nextInt();
+
+            System.out.print("1. Podaj imię: ");
+            name = input.next();
+            System.out.print("2. Podaj email: ");
+            email = input.next();
+            System.out.print("3. Podaj hasło: ");
+            password = input.next();
+            System.out.print("4. Podaj id grupy: ");
+            user_group_id = input.nextInt();
+
+            User user = new User(name, email, password, user_group_id);
+
+            user.setId(userId);
+
+            userDao.update(user);
+
+            System.out.println("Dane zaktualizowane..");
+        }
+
+        else if(choice==3){
+            int userId;
+
+            System.out.println("Usuwanie użytkownika");
+
+            System.out.print("Podaj id użytkownika: ");
+            userId = input.nextInt();
+
+            userDao.delete(userId);
+
+            System.out.println("Użytkownik usunięty..");
+
+        }
+
+        else if(choice==4){
+            System.exit(0);
+        }
+
+        else{
+            System.out.println("Nie oszukuj, nie mam opcji zabezpieczających.. :)");
+        }
+
+    }
+}

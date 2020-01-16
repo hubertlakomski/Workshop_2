c95c5923c95179d4dc58ac29473dafe612d021d1
Hubert Łakomski <hubertlakomski@gmail.com>
2020-01-16 19:04:04 +0100

    add option to login admin

--- src/pl/Application.java
+++ src/pl/Application.java
@@ -1,15 +1,35 @@
 package pl;
 
+import pl.models.menus.adminMenu.AdminMenu;
 import pl.models.menus.userMenu.UserPanel;
+
+import java.util.Scanner;
 
 public class Application {
 
+    private static String ADMIN_PASSWORD = "pass";
+
     public static void main(String[] args) {
 
-        UserPanel userPanel = new UserPanel();
+        String userData = "";
 
         while(true) {
-            userPanel.turn();
+
+            if(!userData.equals(ADMIN_PASSWORD)) {
+                Scanner input = new Scanner(System.in);
+                System.out.print("Jak wiesz co to wpisz: ");
+                userData = input.next();
+            }
+
+            if(userData.equals(ADMIN_PASSWORD)){
+                AdminMenu adminMenu = new AdminMenu();
+                adminMenu.turn();
+            }
+            else {
+                UserPanel userPanel = new UserPanel();
+                userPanel.turn();
+            }
+
         }
 
     }
